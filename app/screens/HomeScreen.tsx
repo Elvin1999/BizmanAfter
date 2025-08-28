92ac081a8ec27913d31f218931777dd217da71b9
Elvin1999 <camalzade_elvin@mail.ru>
2025-08-28 16:14:32 +0400

    done

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,15 +1,37 @@
+import ClientChart from "@/components/charts/ClientChart";
+import { useEffect, useState } from "react";
 import { Button, Text, View } from "react-native";
+import { getClients, initDatabase } from "../db/database";
 import { removeToken } from "../utils/auth";
 
 export default function HomeScreen({ navigation }: any) {
+
+    const [clients, setClients] = useState<any[]>([]);
+
     const logout = async () => {
         await removeToken();
         navigation.replace('Login');
     };
 
+    const loadClients = async () => {
+        await getClients((data) => {
+            setClients(data);
+        })
+    };
+
+    useEffect(() => {
+        (async () => {
+            await initDatabase();
+            await loadClients();
+        })();
+    });
+
+    const clientsData = clients ?? [];
+
     return (
         <View>
             <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Welcome to Bizman!</Text>
+            <ClientChart clients={clientsData} />
             <Button title="Go Clients" onPress={() => navigation.navigate('Clients')}></Button>
             <Button title="Logout" onPress={logout} />
         </View>
---
+++ components/charts/ClientChart.tsx
@@ -0,0 +1,48 @@
+import { getClientSales } from "@/app/db/database";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+import { VictoryPie } from "victory-native";
+
+export default function ClientChart({ clients }: any) {
+    const [chartData, setChartData] = useState<any[]>([]);
+
+    useEffect(() => {
+
+        (async () => {
+            await loadClients();
+        })();
+
+    }, [clients]);
+
+    const loadClients = async () => {
+        const data: any[] = [];
+        for (let client of clients) {
+            await getClientSales(client.id, (sales) => {
+                const totalAmount = sales.reduce((sum, s) => sum + s.amount, 0);
+                if (totalAmount > 0) {
+                    data.push({ x: client.name, y: totalAmount });
+                }
+            })
+        }
+
+        setChartData(data);
+    }
+
+    if (chartData.length === 0) {
+        return <Text style={{ textAlign: 'center', fontSize: 18, fontWeight: 'bold' }}>No data available</Text>
+    }
+
+    return (
+        <View style={{ margin: 3,alignContent:'center',alignItems:'center' }}>
+            <Text>Sales distribution by Client</Text>
+            <VictoryPie
+                data={chartData}
+                colorScale={'qualitative'}
+                labels={({ datum }) => `${datum.x}:${datum.y}`}
+                style={{ labels: { fontSize: 12, padding: 10 } }}
+                height={300}
+            />
+        </View>
+    )
+}
